b33739b3e526cd1f51819be6024f2c76552f4d26
Hendry Lim <myhendry@gmail.com>
2018-09-21 11:31:25 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import HomePage from "./pages/HomePage";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <p>HomePage</p>
+        <Footer />
+      </div>
+    );
+  }
+}
+export default HomePage;
